c41a66f457a9d84c3af96866b2406c5389eb4886
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2017-04-14 14:22:46 -0400

    Support _HOST placeholder in Kerberos internal communication

---
+++ presto-main/src/test/java/com/facebook/presto/util/TestKerberosPrincipal.java
@@ -0,0 +1,117 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.util;
+
+import org.testng.annotations.Test;
+
+import java.util.Optional;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestKerberosPrincipal
+{
+    @Test
+    public void testPrincipalParsing()
+            throws Exception
+    {
+        assertEquals(
+                KerberosPrincipal.valueOf("user"),
+                new KerberosPrincipal("user", Optional.empty(), Optional.empty()));
+        assertEquals(
+                KerberosPrincipal.valueOf("user@REALM.COM"),
+                new KerberosPrincipal("user", Optional.empty(), Optional.of("REALM.COM")));
+        assertEquals(
+                KerberosPrincipal.valueOf("user/hostname@REALM.COM"),
+                new KerberosPrincipal("user", Optional.of("hostname"), Optional.of("REALM.COM")));
+        assertEquals(
+                KerberosPrincipal.valueOf("user\\@host.com@REALM.COM"),
+                new KerberosPrincipal("user\\@host.com", Optional.empty(), Optional.of("REALM.COM")));
+        assertEquals(
+                KerberosPrincipal.valueOf("user/_HOST@REALM.COM"),
+                new KerberosPrincipal("user", Optional.of("_HOST"), Optional.of("REALM.COM")));
+        assertEquals(
+                KerberosPrincipal.valueOf("user/part1/part2/_HOST@REALM.COM"),
+                new KerberosPrincipal("user/part1/part2", Optional.of("_HOST"), Optional.of("REALM.COM")));
+        assertEquals(
+                KerberosPrincipal.valueOf("user\\/part1\\/part2\\/_HOST@REALM.COM"),
+                new KerberosPrincipal("user\\/part1\\/part2\\/_HOST", Optional.empty(), Optional.of("REALM.COM")));
+        assertEquals(
+                KerberosPrincipal.valueOf("user\\/part1\\/part2\\/_HOST\\@REALM.COM"),
+                new KerberosPrincipal("user\\/part1\\/part2\\/_HOST\\@REALM.COM", Optional.empty(), Optional.empty()));
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Invalid Kerberos principal: user/host@")
+    public void testEmptyRealm()
+            throws Exception
+    {
+        KerberosPrincipal.valueOf("user/host@");
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Invalid Kerberos principal: /host")
+    public void testEmptyUsernameWithNoRealm()
+            throws Exception
+    {
+        KerberosPrincipal.valueOf("/host");
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Invalid Kerberos principal: @REALM.COM")
+    public void testEmptyUsernameWithRealm()
+            throws Exception
+    {
+        KerberosPrincipal.valueOf("@REALM.COM");
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Invalid Kerberos principal: user@REALM.COM/host")
+    public void testInvalidPrincipal()
+            throws Exception
+    {
+        KerberosPrincipal.valueOf("user@REALM.COM/host");
+    }
+
+    @Test
+    public void testSubstituteHostnamePlaceholder()
+            throws Exception
+    {
+        assertEquals(
+                KerberosPrincipal.valueOf("user").substituteHostnamePlaceholder("localhost"),
+                new KerberosPrincipal("user", Optional.empty(), Optional.empty()));
+        assertEquals(
+                KerberosPrincipal.valueOf("user/_HOST").substituteHostnamePlaceholder("localhost"),
+                new KerberosPrincipal("user", Optional.of("localhost"), Optional.empty()));
+        assertEquals(
+                KerberosPrincipal.valueOf("user/_HOST@REALM.COM").substituteHostnamePlaceholder("localhost"),
+                new KerberosPrincipal("user", Optional.of("localhost"), Optional.of("REALM.COM")));
+        assertEquals(
+                KerberosPrincipal.valueOf("user@REALM.COM").substituteHostnamePlaceholder("localhost"),
+                new KerberosPrincipal("user", Optional.empty(), Optional.of("REALM.COM")));
+    }
+
+    @Test
+    public void testToString()
+            throws Exception
+    {
+        assertEquals(
+                new KerberosPrincipal("user", Optional.empty(), Optional.empty()).toString(),
+                "user");
+        assertEquals(
+                new KerberosPrincipal("user", Optional.of("host"), Optional.empty()).toString(),
+                "user/host");
+        assertEquals(
+                new KerberosPrincipal("user", Optional.of("host"), Optional.of("REALM.COM")).toString(),
+                "user/host@REALM.COM");
+        assertEquals(
+                new KerberosPrincipal("user\\@host.com", Optional.of("_HOST\\@ESC.COM"), Optional.of("REALM.COM")).toString(),
+                "user\\@host.com/_HOST\\@ESC.COM@REALM.COM");
+    }
+}
