9235a9d811b28c8652b86d80855cdf9ae1067254
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2017-11-27 12:19:51 +0100

    Add new date time semantics to date time cast operators

--- presto-main/src/test/java/com/facebook/presto/type/TestTimestamp.java
+++ presto-main/src/test/java/com/facebook/presto/type/TestTimestamp.java
@@ -13,11 +13,53 @@
  */
 package com.facebook.presto.type;
 
+import org.testng.annotations.Test;
+
+import static com.facebook.presto.spi.StandardErrorCode.INVALID_CAST_ARGUMENT;
+
 public class TestTimestamp
         extends TestTimestampBase
 {
     public TestTimestamp()
     {
         super(false);
+    }
+
+    @Test
+    public void testCastFromSliceFailsOnTimestampWithTimeZone()
+    {
+        assertInvalidFunction(
+                "cast('2001-1-22 03:04:05.321 +07:09' as timestamp)",
+                INVALID_CAST_ARGUMENT,
+                "Value cannot be cast to timestamp as it contains explicitly defined TIME ZONE: 2001-1-22 03:04:05.321 +07:09");
+        assertInvalidFunction(
+                "cast('2001-1-22 03:04:05 +07:09' as timestamp)",
+                INVALID_CAST_ARGUMENT,
+                "Value cannot be cast to timestamp as it contains explicitly defined TIME ZONE: 2001-1-22 03:04:05 +07:09");
+        assertInvalidFunction(
+                "cast('2001-1-22 03:04 +07:09' as timestamp)",
+                INVALID_CAST_ARGUMENT,
+                "Value cannot be cast to timestamp as it contains explicitly defined TIME ZONE: 2001-1-22 03:04 +07:09");
+        assertInvalidFunction(
+                "cast('2001-1-22 +07:09' as timestamp)",
+                INVALID_CAST_ARGUMENT,
+                "Value cannot be cast to timestamp as it contains explicitly defined TIME ZONE: 2001-1-22 +07:09");
+
+        assertInvalidFunction(
+                "cast('2001-1-22 03:04:05.321 Asia/Oral' as timestamp)",
+                INVALID_CAST_ARGUMENT,
+                "Value cannot be cast to timestamp as it contains explicitly defined TIME ZONE: 2001-1-22 03:04:05.321 Asia/Oral");
+        assertInvalidFunction(
+                "cast('2001-1-22 03:04:05 Asia/Oral' as timestamp)",
+                INVALID_CAST_ARGUMENT,
+                "Value cannot be cast to timestamp as it contains explicitly defined TIME ZONE: 2001-1-22 03:04:05 Asia/Oral");
+        assertInvalidFunction(
+                "cast('2001-1-22 03:04 Asia/Oral' as timestamp)",
+                INVALID_CAST_ARGUMENT,
+                "Value cannot be cast to timestamp as it contains explicitly defined TIME ZONE: 2001-1-22 03:04 Asia/Oral");
+        assertInvalidFunction(
+                "cast('2001-1-22 Asia/Oral' as timestamp)",
+                INVALID_CAST_ARGUMENT,
+                "Value cannot be cast to timestamp as it contains explicitly defined TIME ZONE: 2001-1-22 Asia/Oral");
     }
 }
