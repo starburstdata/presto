starburstdata/facebook-presto

Add JoinStatsRule

presto-main/src/main/java/com/facebook/presto/server/ServerMainModule.java
@@ -36,6 +36,7 @@ import com.facebook.presto.cost.EnsureStatsMatchOutput;
 import com.facebook.presto.cost.ExchangeStatsRule;
 import com.facebook.presto.cost.FilterStatsCalculator;
 import com.facebook.presto.cost.FilterStatsRule;
+import com.facebook.presto.cost.JoinStatsRule;
 import com.facebook.presto.cost.LimitStatsRule;
 import com.facebook.presto.cost.OutputStatsRule;
 import com.facebook.presto.cost.ProjectStatsRule;
@@ -516,6 +517,7 @@ public class ServerMainModule
         rules.add(new EnforceSingleRowStatsRule());
         rules.add(new ExchangeStatsRule());
         rules.add(new ProjectStatsRule(scalarStatsCalculator));
+        rules.add(new JoinStatsRule(filterStatsCalculator));
 
         ImmutableList.Builder<ComposableStatsCalculator.Normalizer> normalizers = ImmutableList.builder();
         normalizers.add(new EnsureStatsMatchOutput());

starburstdata/presto

presto-main/src/main/java/com/facebook/presto/server/ServerMainModule.java
@@ -33,6 +33,7 @@ import com.facebook.presto.cost.EnforceSingleRowStatsRule;
 import com.facebook.presto.cost.ExchangeStatsRule;
 import com.facebook.presto.cost.FilterStatsCalculator;
 import com.facebook.presto.cost.FilterStatsRule;
+import com.facebook.presto.cost.JoinStatsRule;
 import com.facebook.presto.cost.LimitStatsRule;
 import com.facebook.presto.cost.OutputStatsRule;
 import com.facebook.presto.cost.ProjectStatsRule;
@@ -529,16 +530,18 @@ public class ServerMainModule
     {
         StatsNormalizer normalizer = new StatsNormalizer();
         ScalarStatsCalculator scalarStatsCalculator = new ScalarStatsCalculator(metadata);
+        FilterStatsCalculator filterStatsCalculator = new FilterStatsCalculator(metadata, scalarStatsCalculator, normalizer);
 
         ImmutableList.Builder<ComposableStatsCalculator.Rule> rules = ImmutableList.builder();
         rules.add(new OutputStatsRule());
         rules.add(new TableScanStatsRule(metadata, normalizer));
-        rules.add(new FilterStatsRule(new FilterStatsCalculator(metadata, scalarStatsCalculator, normalizer)));
+        rules.add(new FilterStatsRule(filterStatsCalculator));
         rules.add(new ValuesStatsRule(metadata));
         rules.add(new LimitStatsRule(normalizer));
         rules.add(new EnforceSingleRowStatsRule(normalizer));
         rules.add(new ProjectStatsRule(scalarStatsCalculator, normalizer));
         rules.add(new ExchangeStatsRule(normalizer));
+        rules.add(new JoinStatsRule(filterStatsCalculator, normalizer));
 
         return new ComposableStatsCalculator(rules.build());
     }