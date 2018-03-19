starburstdata/facebook-presto

Replace distributed_join with join_distribution_type session property
The `join_distribution_type` property has three option: "REPARTITIONED",
"REPLICATED", and "AUTOMATIC". This property replaces the
`distributed_join` property. "REPARTITIONED" has the same behavior as
`distribtued_join=true`. "REPLICATED" is equivalent to
`distributed_join=false`. "AUTOMATIC" will use stats to evaluate whether
partitioned or replicated is better, or if no information is available,
it will fall back to partitioned.

The default value for `join_distribution_type` is "REPARTITIONED".

presto-product-tests/src/main/java/com/facebook/presto/tests/cli/PrestoCliTests.java
@@ -211,14 +211,14 @@ public class PrestoCliTests
 
         presto.getProcessInput().println("show session;");
         assertThat(squeezeLines(presto.readLinesUntilPrompt()))
-                .contains("distributed_join|true|true|boolean|Use a distributed join instead of a broadcast join");
+                .contains("join_distribution_type|REPARTITIONED|REPARTITIONED|varchar|The join method to use. Options are AUTOMATIC,REPLICATED,REPARTITIONED");
 
-        presto.getProcessInput().println("set session distributed_join = false;");
+        presto.getProcessInput().println("set session join_distribution_type = 'REPLICATED';");
         assertThat(presto.readLinesUntilPrompt()).contains("SET SESSION");
 
         presto.getProcessInput().println("show session;");
         assertThat(squeezeLines(presto.readLinesUntilPrompt()))
-                .contains("distributed_join|false|true|boolean|Use a distributed join instead of a broadcast join");
+                .contains("join_distribution_type|REPLICATED|REPARTITIONED|varchar|The join method to use. Options are AUTOMATIC,REPLICATED,REPARTITIONED");
     }
 
     @Test(groups = CLI, timeOut = TIMEOUT)

starburstdata/presto

presto-product-tests/src/main/java/com/facebook/presto/tests/cli/PrestoCliTests.java
@@ -222,14 +222,14 @@ public class PrestoCliTests
 
         presto.getProcessInput().println("show session;");
         assertThat(squeezeLines(presto.readLinesUntilPrompt()))
-                .contains("distributed_join|true|true|boolean|Use a distributed join instead of a broadcast join");
+                .contains("join_distribution_type|REPARTITIONED|REPARTITIONED|varchar|The join method to use. Options are REPLICATED,REPARTITIONED,AUTOMATIC");
 
-        presto.getProcessInput().println("set session distributed_join = false;");
+        presto.getProcessInput().println("set session join_distribution_type = 'REPLICATED';");
         assertThat(presto.readLinesUntilPrompt()).contains("SET SESSION");
 
         presto.getProcessInput().println("show session;");
         assertThat(squeezeLines(presto.readLinesUntilPrompt()))
-                .contains("distributed_join|false|true|boolean|Use a distributed join instead of a broadcast join");
+                .contains("join_distribution_type|REPLICATED|REPARTITIONED|varchar|The join method to use. Options are REPLICATED,REPARTITIONED,AUTOMATIC");
     }
 
     @Test(groups = CLI, timeOut = TIMEOUT)