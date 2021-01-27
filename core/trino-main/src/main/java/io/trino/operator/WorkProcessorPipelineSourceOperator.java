ringtail/presto-1

Merge materialized output pages

presto-main/src/main/java/io/prestosql/operator/WorkProcessorPipelineSourceOperator.java
@@ -28,6 +28,7 @@ import io.prestosql.operator.OperationTimer.OperationTiming;
 import io.prestosql.operator.WorkProcessor.ProcessState;
 import io.prestosql.spi.Page;
 import io.prestosql.spi.connector.UpdatablePageSource;
+import io.prestosql.spi.type.Type;
 import io.prestosql.sql.planner.LocalExecutionPlanner.OperatorFactoryWithTypes;
 import io.prestosql.sql.planner.plan.PlanNodeId;
 
@@ -48,6 +49,7 @@ import static io.prestosql.operator.BlockedReason.WAITING_FOR_MEMORY;
 import static io.prestosql.operator.PageUtils.recordMaterializedBytes;
 import static io.prestosql.operator.WorkProcessor.ProcessState.Type.BLOCKED;
 import static io.prestosql.operator.WorkProcessor.ProcessState.Type.FINISHED;
+import static io.prestosql.operator.project.MergePages.mergePages;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
@@ -72,7 +74,11 @@ public class WorkProcessorPipelineSourceOperator
     private SettableFuture<?> blockedOnSplits = SettableFuture.create();
     private boolean operatorFinishing;
 
-    public static List<OperatorFactory> convertOperators(int operatorId, List<OperatorFactoryWithTypes> operatorFactoriesWithTypes)
+    public static List<OperatorFactory> convertOperators(
+            int operatorId,
+            List<OperatorFactoryWithTypes> operatorFactoriesWithTypes,
+            DataSize minOutputPageSize,
+            int minOutputPageRowCount)
     {
         if (operatorFactoriesWithTypes.isEmpty() || !(operatorFactoriesWithTypes.get(0).getOperatorFactory() instanceof WorkProcessorSourceOperatorFactory)) {
             return toOperatorFactories(operatorFactoriesWithTypes);
@@ -95,7 +101,13 @@ public class WorkProcessorPipelineSourceOperator
         }
 
         return ImmutableList.<OperatorFactory>builder()
-                .add(new WorkProcessorPipelineSourceOperatorFactory(operatorId, sourceOperatorFactory, workProcessorOperatorFactories))
+                .add(new WorkProcessorPipelineSourceOperatorFactory(
+                        operatorId,
+                        sourceOperatorFactory,
+                        workProcessorOperatorFactories,
+                        operatorFactoriesWithTypes.get(operatorIndex - 1).getTypes(),
+                        minOutputPageSize,
+                        minOutputPageRowCount))
                 .addAll(toOperatorFactories(operatorFactoriesWithTypes.subList(operatorIndex, operatorFactoriesWithTypes.size())))
                 .build();
     }
@@ -111,7 +123,10 @@ public class WorkProcessorPipelineSourceOperator
             int operatorId,
             DriverContext driverContext,
             WorkProcessorSourceOperatorFactory sourceOperatorFactory,
-            List<WorkProcessorOperatorFactory> operatorFactories)
+            List<WorkProcessorOperatorFactory> operatorFactories,
+            List<Type> outputTypes,
+            DataSize minOutputPageSize,
+            int minOutputPageRowCount)
     {
         requireNonNull(driverContext, "driverContext is null");
         requireNonNull(sourceOperatorFactory, "sourceOperatorFactory is null");
@@ -170,6 +185,12 @@ public class WorkProcessorPipelineSourceOperator
 
         // materialize output pages as there are no semantics guarantees for non WorkProcessor operators
         pages = pages.map(Page::getLoadedPage);
+        pages = pages.transformProcessor(processor -> mergePages(
+                outputTypes,
+                minOutputPageSize.toBytes(),
+                minOutputPageRowCount,
+                processor,
+                operatorContext.aggregateUserMemoryContext()));
 
         // finish early when entire pipeline is closed
         this.pages = pages.finishWhen(() -> operatorFinishing);
@@ -699,16 +720,25 @@ public class WorkProcessorPipelineSourceOperator
         private final int operatorId;
         private final WorkProcessorSourceOperatorFactory sourceOperatorFactory;
         private final List<WorkProcessorOperatorFactory> operatorFactories;
+        private final List<Type> outputTypes;
+        private final DataSize minOutputPageSize;
+        private final int minOutputPageRowCount;
         private boolean closed;
 
         private WorkProcessorPipelineSourceOperatorFactory(
                 int operatorId,
                 WorkProcessorSourceOperatorFactory sourceOperatorFactory,
-                List<WorkProcessorOperatorFactory> operatorFactories)
+                List<WorkProcessorOperatorFactory> operatorFactories,
+                List<Type> outputTypes,
+                DataSize minOutputPageSize,
+                int minOutputPageRowCount)
         {
             this.operatorId = operatorId;
             this.sourceOperatorFactory = requireNonNull(sourceOperatorFactory, "sourceOperatorFactory is null");
             this.operatorFactories = requireNonNull(operatorFactories, "operatorFactories is null");
+            this.outputTypes = requireNonNull(outputTypes, "outputTypes is null");
+            this.minOutputPageSize = requireNonNull(minOutputPageSize, "minOutputPageSize is null");
+            this.minOutputPageRowCount = minOutputPageRowCount;
         }
 
         @Override
@@ -721,7 +751,14 @@ public class WorkProcessorPipelineSourceOperator
         public SourceOperator createOperator(DriverContext driverContext)
         {
             checkState(!closed, "Factory is already closed");
-            return new WorkProcessorPipelineSourceOperator(operatorId, driverContext, sourceOperatorFactory, operatorFactories);
+            return new WorkProcessorPipelineSourceOperator(
+                    operatorId,
+                    driverContext,
+                    sourceOperatorFactory,
+                    operatorFactories,
+                    outputTypes,
+                    minOutputPageSize,
+                    minOutputPageRowCount);
         }
 
         @Override

starburstdata/presto

core/trino-main/src/main/java/io/trino/operator/WorkProcessorPipelineSourceOperator.java
@@ -28,6 +28,7 @@ import io.trino.operator.OperationTimer.OperationTiming;
 import io.trino.operator.WorkProcessor.ProcessState;
 import io.trino.spi.Page;
 import io.trino.spi.connector.UpdatablePageSource;
+import io.trino.spi.type.Type;
 import io.trino.sql.planner.LocalExecutionPlanner.OperatorFactoryWithTypes;
 import io.trino.sql.planner.plan.PlanNodeId;
 
@@ -48,6 +49,7 @@ import static io.trino.operator.BlockedReason.WAITING_FOR_MEMORY;
 import static io.trino.operator.PageUtils.recordMaterializedBytes;
 import static io.trino.operator.WorkProcessor.ProcessState.Type.BLOCKED;
 import static io.trino.operator.WorkProcessor.ProcessState.Type.FINISHED;
+import static io.trino.operator.project.MergePages.mergePages;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
@@ -72,7 +74,11 @@ public class WorkProcessorPipelineSourceOperator
     private SettableFuture<?> blockedOnSplits = SettableFuture.create();
     private boolean operatorFinishing;
 
-    public static List<OperatorFactory> convertOperators(int operatorId, List<OperatorFactoryWithTypes> operatorFactoriesWithTypes)
+    public static List<OperatorFactory> convertOperators(
+            int operatorId,
+            List<OperatorFactoryWithTypes> operatorFactoriesWithTypes,
+            DataSize minOutputPageSize,
+            int minOutputPageRowCount)
     {
         if (operatorFactoriesWithTypes.isEmpty() || !(operatorFactoriesWithTypes.get(0).getOperatorFactory() instanceof WorkProcessorSourceOperatorFactory)) {
             return toOperatorFactories(operatorFactoriesWithTypes);
@@ -95,7 +101,13 @@ public class WorkProcessorPipelineSourceOperator
         }
 
         return ImmutableList.<OperatorFactory>builder()
-                .add(new WorkProcessorPipelineSourceOperatorFactory(operatorId, sourceOperatorFactory, workProcessorOperatorFactories))
+                .add(new WorkProcessorPipelineSourceOperatorFactory(
+                        operatorId,
+                        sourceOperatorFactory,
+                        workProcessorOperatorFactories,
+                        operatorFactoriesWithTypes.get(operatorIndex - 1).getTypes(),
+                        minOutputPageSize,
+                        minOutputPageRowCount))
                 .addAll(toOperatorFactories(operatorFactoriesWithTypes.subList(operatorIndex, operatorFactoriesWithTypes.size())))
                 .build();
     }
@@ -111,7 +123,10 @@ public class WorkProcessorPipelineSourceOperator
             int operatorId,
             DriverContext driverContext,
             WorkProcessorSourceOperatorFactory sourceOperatorFactory,
-            List<WorkProcessorOperatorFactory> operatorFactories)
+            List<WorkProcessorOperatorFactory> operatorFactories,
+            List<Type> outputTypes,
+            DataSize minOutputPageSize,
+            int minOutputPageRowCount)
     {
         requireNonNull(driverContext, "driverContext is null");
         requireNonNull(sourceOperatorFactory, "sourceOperatorFactory is null");
@@ -170,6 +185,12 @@ public class WorkProcessorPipelineSourceOperator
 
         // materialize output pages as there are no semantics guarantees for non WorkProcessor operators
         pages = pages.map(Page::getLoadedPage);
+        pages = pages.transformProcessor(processor -> mergePages(
+                outputTypes,
+                minOutputPageSize.toBytes(),
+                minOutputPageRowCount,
+                processor,
+                operatorContext.aggregateUserMemoryContext()));
 
         // finish early when entire pipeline is closed
         this.pages = pages.finishWhen(() -> operatorFinishing);
@@ -699,16 +720,25 @@ public class WorkProcessorPipelineSourceOperator
         private final int operatorId;
         private final WorkProcessorSourceOperatorFactory sourceOperatorFactory;
         private final List<WorkProcessorOperatorFactory> operatorFactories;
+        private final List<Type> outputTypes;
+        private final DataSize minOutputPageSize;
+        private final int minOutputPageRowCount;
         private boolean closed;
 
         private WorkProcessorPipelineSourceOperatorFactory(
                 int operatorId,
                 WorkProcessorSourceOperatorFactory sourceOperatorFactory,
-                List<WorkProcessorOperatorFactory> operatorFactories)
+                List<WorkProcessorOperatorFactory> operatorFactories,
+                List<Type> outputTypes,
+                DataSize minOutputPageSize,
+                int minOutputPageRowCount)
         {
             this.operatorId = operatorId;
             this.sourceOperatorFactory = requireNonNull(sourceOperatorFactory, "sourceOperatorFactory is null");
             this.operatorFactories = requireNonNull(operatorFactories, "operatorFactories is null");
+            this.outputTypes = requireNonNull(outputTypes, "outputTypes is null");
+            this.minOutputPageSize = requireNonNull(minOutputPageSize, "minOutputPageSize is null");
+            this.minOutputPageRowCount = minOutputPageRowCount;
         }
 
         @Override
@@ -721,7 +751,14 @@ public class WorkProcessorPipelineSourceOperator
         public SourceOperator createOperator(DriverContext driverContext)
         {
             checkState(!closed, "Factory is already closed");
-            return new WorkProcessorPipelineSourceOperator(operatorId, driverContext, sourceOperatorFactory, operatorFactories);
+            return new WorkProcessorPipelineSourceOperator(
+                    operatorId,
+                    driverContext,
+                    sourceOperatorFactory,
+                    operatorFactories,
+                    outputTypes,
+                    minOutputPageSize,
+                    minOutputPageRowCount);
         }
 
         @Override