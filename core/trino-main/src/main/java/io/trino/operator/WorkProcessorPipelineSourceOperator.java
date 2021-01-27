ringtail/presto-1

Pass type information to WorkProcessorPipelineSourceOperator

presto-main/src/main/java/io/prestosql/operator/WorkProcessorPipelineSourceOperator.java
@@ -28,6 +28,7 @@ import io.prestosql.operator.OperationTimer.OperationTiming;
 import io.prestosql.operator.WorkProcessor.ProcessState;
 import io.prestosql.spi.Page;
 import io.prestosql.spi.connector.UpdatablePageSource;
+import io.prestosql.sql.planner.LocalExecutionPlanner.OperatorFactoryWithTypes;
 import io.prestosql.sql.planner.plan.PlanNodeId;
 
 import javax.annotation.Nullable;
@@ -71,31 +72,39 @@ public class WorkProcessorPipelineSourceOperator
     private SettableFuture<?> blockedOnSplits = SettableFuture.create();
     private boolean operatorFinishing;
 
-    public static List<OperatorFactory> convertOperators(int operatorId, List<OperatorFactory> operatorFactories)
+    public static List<OperatorFactory> convertOperators(int operatorId, List<OperatorFactoryWithTypes> operatorFactoriesWithTypes)
     {
-        if (operatorFactories.isEmpty() || !(operatorFactories.get(0) instanceof WorkProcessorSourceOperatorFactory)) {
-            return operatorFactories;
+        if (operatorFactoriesWithTypes.isEmpty() || !(operatorFactoriesWithTypes.get(0).getOperatorFactory() instanceof WorkProcessorSourceOperatorFactory)) {
+            return toOperatorFactories(operatorFactoriesWithTypes);
         }
 
-        WorkProcessorSourceOperatorFactory sourceOperatorFactory = (WorkProcessorSourceOperatorFactory) operatorFactories.get(0);
+        WorkProcessorSourceOperatorFactory sourceOperatorFactory = (WorkProcessorSourceOperatorFactory) operatorFactoriesWithTypes.get(0).getOperatorFactory();
         ImmutableList.Builder<WorkProcessorOperatorFactory> workProcessorOperatorFactoriesBuilder = ImmutableList.builder();
         int operatorIndex = 1;
-        for (; operatorIndex < operatorFactories.size(); ++operatorIndex) {
-            if (!(operatorFactories.get(operatorIndex) instanceof WorkProcessorOperatorFactory)) {
+        for (; operatorIndex < operatorFactoriesWithTypes.size(); ++operatorIndex) {
+            OperatorFactory operatorFactory = operatorFactoriesWithTypes.get(operatorIndex).getOperatorFactory();
+            if (!(operatorFactory instanceof WorkProcessorOperatorFactory)) {
                 break;
             }
-            workProcessorOperatorFactoriesBuilder.add((WorkProcessorOperatorFactory) operatorFactories.get(operatorIndex));
+            workProcessorOperatorFactoriesBuilder.add((WorkProcessorOperatorFactory) operatorFactory);
         }
 
         List<WorkProcessorOperatorFactory> workProcessorOperatorFactories = workProcessorOperatorFactoriesBuilder.build();
         if (workProcessorOperatorFactories.isEmpty()) {
-            return operatorFactories;
+            return toOperatorFactories(operatorFactoriesWithTypes);
         }
 
         return ImmutableList.<OperatorFactory>builder()
                 .add(new WorkProcessorPipelineSourceOperatorFactory(operatorId, sourceOperatorFactory, workProcessorOperatorFactories))
-                .addAll(operatorFactories.subList(operatorIndex, operatorFactories.size()))
+                .addAll(toOperatorFactories(operatorFactoriesWithTypes.subList(operatorIndex, operatorFactoriesWithTypes.size())))
                 .build();
+    }
+
+    private static List<OperatorFactory> toOperatorFactories(List<OperatorFactoryWithTypes> operatorFactoryWithTypes)
+    {
+        return operatorFactoryWithTypes.stream()
+                .map(OperatorFactoryWithTypes::getOperatorFactory)
+                .collect(toImmutableList());
     }
 
     private WorkProcessorPipelineSourceOperator(

presto-main/src/main/java/io/prestosql/sql/planner/LocalExecutionPlanner.java
@@ -563,21 +563,42 @@ public class LocalExecutionPlanner
 
         public void addDriverFactory(boolean inputDriver, boolean outputDriver, PhysicalOperation physicalOperation, OperatorFactory finalOperatorFactory, OptionalInt driverInstances, PipelineExecutionStrategy pipelineExecutionStrategy)
         {
-            addDriverFactory(
-                    inputDriver,
-                    outputDriver,
-                    ImmutableList.<OperatorFactory>builder()
-                            .addAll(physicalOperation.getOperatorFactories())
-                            .add(finalOperatorFactory)
-                            .build(),
-                    driverInstances,
-                    pipelineExecutionStrategy);
+            List<OperatorFactoryWithTypes> operatorFactoryWithTypes = ImmutableList.<OperatorFactoryWithTypes>builder()
+                    .addAll(physicalOperation.getOperatorFactoriesWithTypes())
+                    .add(new OperatorFactoryWithTypes(
+                            finalOperatorFactory,
+                            // final operator doesn't produce any pages
+                            ImmutableList.of()))
+                    .build();
+
+            validateFirstOperatorFactory(inputDriver, operatorFactoryWithTypes.get(0).getOperatorFactory(), pipelineExecutionStrategy);
+
+            List<OperatorFactory> operatorFactories = handleLateMaterialization(operatorFactoryWithTypes);
+
+            driverFactories.add(new DriverFactory(getNextPipelineId(), inputDriver, outputDriver, operatorFactories, driverInstances, pipelineExecutionStrategy));
+        }
+
+        private List<OperatorFactory> handleLateMaterialization(List<OperatorFactoryWithTypes> operatorFactoryWithTypes)
+        {
+            if (isLateMaterializationEnabled(taskContext.getSession())) {
+                return WorkProcessorPipelineSourceOperator.convertOperators(getNextOperatorId(), operatorFactoryWithTypes);
+            }
+            else {
+                return operatorFactoryWithTypes.stream()
+                        .map(OperatorFactoryWithTypes::getOperatorFactory)
+                        .collect(toImmutableList());
+            }
         }
 
         public void addDriverFactory(boolean inputDriver, boolean outputDriver, List<OperatorFactory> operatorFactories, OptionalInt driverInstances, PipelineExecutionStrategy pipelineExecutionStrategy)
         {
+            validateFirstOperatorFactory(inputDriver, operatorFactories.get(0), pipelineExecutionStrategy);
+            driverFactories.add(new DriverFactory(getNextPipelineId(), inputDriver, outputDriver, operatorFactories, driverInstances, pipelineExecutionStrategy));
+        }
+
+        private void validateFirstOperatorFactory(boolean inputDriver, OperatorFactory firstOperatorFactory, PipelineExecutionStrategy pipelineExecutionStrategy)
+        {
             if (pipelineExecutionStrategy == GROUPED_EXECUTION) {
-                OperatorFactory firstOperatorFactory = operatorFactories.get(0);
                 if (inputDriver) {
                     checkArgument(firstOperatorFactory instanceof ScanFilterAndProjectOperatorFactory || firstOperatorFactory instanceof TableScanOperatorFactory);
                 }
@@ -585,12 +606,6 @@ public class LocalExecutionPlanner
                     checkArgument(firstOperatorFactory instanceof LocalExchangeSourceOperatorFactory || firstOperatorFactory instanceof LookupOuterOperatorFactory);
                 }
             }
-
-            if (isLateMaterializationEnabled(taskContext.getSession())) {
-                operatorFactories = WorkProcessorPipelineSourceOperator.convertOperators(getNextOperatorId(), operatorFactories);
-            }
-
-            driverFactories.add(new DriverFactory(getNextPipelineId(), inputDriver, outputDriver, operatorFactories, driverInstances, pipelineExecutionStrategy));
         }
 
         private List<DriverFactory> getDriverFactories()
@@ -710,6 +725,28 @@ public class LocalExecutionPlanner
         public StageExecutionDescriptor getStageExecutionDescriptor()
         {
             return stageExecutionDescriptor;
+        }
+    }
+
+    public static class OperatorFactoryWithTypes
+    {
+        private final OperatorFactory operatorFactory;
+        private final List<Type> types;
+
+        public OperatorFactoryWithTypes(OperatorFactory operatorFactory, List<Type> types)
+        {
+            this.operatorFactory = requireNonNull(operatorFactory, "operatorFactory is null");
+            this.types = requireNonNull(types, "types is null");
+        }
+
+        public OperatorFactory getOperatorFactory()
+        {
+            return operatorFactory;
+        }
+
+        public List<Type> getTypes()
+        {
+            return types;
         }
     }
 
@@ -2965,7 +3002,7 @@ public class LocalExecutionPlanner
      */
     private static class PhysicalOperation
     {
-        private final List<OperatorFactory> operatorFactories;
+        private final List<OperatorFactoryWithTypes> operatorFactoriesWithTypes;
         private final Map<Symbol, Integer> layout;
         private final List<Type> types;
 
@@ -2994,12 +3031,12 @@ public class LocalExecutionPlanner
             requireNonNull(source, "source is null");
             requireNonNull(pipelineExecutionStrategy, "pipelineExecutionStrategy is null");
 
-            this.operatorFactories = ImmutableList.<OperatorFactory>builder()
-                    .addAll(source.map(PhysicalOperation::getOperatorFactories).orElse(ImmutableList.of()))
-                    .add(operatorFactory)
+            this.types = toTypes(layout, context);
+            this.operatorFactoriesWithTypes = ImmutableList.<OperatorFactoryWithTypes>builder()
+                    .addAll(source.map(PhysicalOperation::getOperatorFactoriesWithTypes).orElse(ImmutableList.of()))
+                    .add(new OperatorFactoryWithTypes(operatorFactory, types))
                     .build();
             this.layout = ImmutableMap.copyOf(layout);
-            this.types = toTypes(layout, context);
             this.pipelineExecutionStrategy = pipelineExecutionStrategy;
         }
 
@@ -3036,7 +3073,14 @@ public class LocalExecutionPlanner
 
         private List<OperatorFactory> getOperatorFactories()
         {
-            return operatorFactories;
+            return operatorFactoriesWithTypes.stream()
+                    .map(OperatorFactoryWithTypes::getOperatorFactory)
+                    .collect(toImmutableList());
+        }
+
+        private List<OperatorFactoryWithTypes> getOperatorFactoriesWithTypes()
+        {
+            return operatorFactoriesWithTypes;
         }
 
         public PipelineExecutionStrategy getPipelineExecutionStrategy()

presto-main/src/test/java/io/prestosql/operator/TestWorkProcessorPipelineSourceOperator.java
@@ -26,6 +26,7 @@ import io.prestosql.operator.WorkProcessor.TransformationState;
 import io.prestosql.operator.WorkProcessorAssertion.Transform;
 import io.prestosql.spi.Page;
 import io.prestosql.spi.connector.UpdatablePageSource;
+import io.prestosql.sql.planner.LocalExecutionPlanner.OperatorFactoryWithTypes;
 import io.prestosql.sql.planner.plan.PlanNodeId;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -105,7 +106,10 @@ public class TestWorkProcessorPipelineSourceOperator
 
         SourceOperatorFactory pipelineOperatorFactory = (SourceOperatorFactory) getOnlyElement(WorkProcessorPipelineSourceOperator.convertOperators(
                 99,
-                ImmutableList.of(sourceOperatorFactory, firstOperatorFactory, secondOperatorFactory)));
+                ImmutableList.of(
+                        new OperatorFactoryWithTypes(sourceOperatorFactory, ImmutableList.of(BIGINT)),
+                        new OperatorFactoryWithTypes(firstOperatorFactory, ImmutableList.of(BIGINT)),
+                        new OperatorFactoryWithTypes(secondOperatorFactory, ImmutableList.of(BIGINT)))));
 
         DriverContext driverContext = TestingOperatorContext.create(scheduledExecutor).getDriverContext();
         SourceOperator pipelineOperator = pipelineOperatorFactory.createOperator(driverContext);

starburstdata/presto

core/trino-main/src/main/java/io/trino/operator/WorkProcessorPipelineSourceOperator.java
@@ -28,6 +28,7 @@ import io.trino.operator.OperationTimer.OperationTiming;
 import io.trino.operator.WorkProcessor.ProcessState;
 import io.trino.spi.Page;
 import io.trino.spi.connector.UpdatablePageSource;
+import io.trino.sql.planner.LocalExecutionPlanner.OperatorFactoryWithTypes;
 import io.trino.sql.planner.plan.PlanNodeId;
 
 import javax.annotation.Nullable;
@@ -71,31 +72,39 @@ public class WorkProcessorPipelineSourceOperator
     private SettableFuture<?> blockedOnSplits = SettableFuture.create();
     private boolean operatorFinishing;
 
-    public static List<OperatorFactory> convertOperators(int operatorId, List<OperatorFactory> operatorFactories)
+    public static List<OperatorFactory> convertOperators(int operatorId, List<OperatorFactoryWithTypes> operatorFactoriesWithTypes)
     {
-        if (operatorFactories.isEmpty() || !(operatorFactories.get(0) instanceof WorkProcessorSourceOperatorFactory)) {
-            return operatorFactories;
+        if (operatorFactoriesWithTypes.isEmpty() || !(operatorFactoriesWithTypes.get(0).getOperatorFactory() instanceof WorkProcessorSourceOperatorFactory)) {
+            return toOperatorFactories(operatorFactoriesWithTypes);
         }
 
-        WorkProcessorSourceOperatorFactory sourceOperatorFactory = (WorkProcessorSourceOperatorFactory) operatorFactories.get(0);
+        WorkProcessorSourceOperatorFactory sourceOperatorFactory = (WorkProcessorSourceOperatorFactory) operatorFactoriesWithTypes.get(0).getOperatorFactory();
         ImmutableList.Builder<WorkProcessorOperatorFactory> workProcessorOperatorFactoriesBuilder = ImmutableList.builder();
         int operatorIndex = 1;
-        for (; operatorIndex < operatorFactories.size(); ++operatorIndex) {
-            if (!(operatorFactories.get(operatorIndex) instanceof WorkProcessorOperatorFactory)) {
+        for (; operatorIndex < operatorFactoriesWithTypes.size(); ++operatorIndex) {
+            OperatorFactory operatorFactory = operatorFactoriesWithTypes.get(operatorIndex).getOperatorFactory();
+            if (!(operatorFactory instanceof WorkProcessorOperatorFactory)) {
                 break;
             }
-            workProcessorOperatorFactoriesBuilder.add((WorkProcessorOperatorFactory) operatorFactories.get(operatorIndex));
+            workProcessorOperatorFactoriesBuilder.add((WorkProcessorOperatorFactory) operatorFactory);
         }
 
         List<WorkProcessorOperatorFactory> workProcessorOperatorFactories = workProcessorOperatorFactoriesBuilder.build();
         if (workProcessorOperatorFactories.isEmpty()) {
-            return operatorFactories;
+            return toOperatorFactories(operatorFactoriesWithTypes);
         }
 
         return ImmutableList.<OperatorFactory>builder()
                 .add(new WorkProcessorPipelineSourceOperatorFactory(operatorId, sourceOperatorFactory, workProcessorOperatorFactories))
-                .addAll(operatorFactories.subList(operatorIndex, operatorFactories.size()))
+                .addAll(toOperatorFactories(operatorFactoriesWithTypes.subList(operatorIndex, operatorFactoriesWithTypes.size())))
                 .build();
+    }
+
+    private static List<OperatorFactory> toOperatorFactories(List<OperatorFactoryWithTypes> operatorFactoryWithTypes)
+    {
+        return operatorFactoryWithTypes.stream()
+                .map(OperatorFactoryWithTypes::getOperatorFactory)
+                .collect(toImmutableList());
     }
 
     private WorkProcessorPipelineSourceOperator(

core/trino-main/src/main/java/io/trino/sql/planner/LocalExecutionPlanner.java
@@ -592,18 +592,33 @@ public class LocalExecutionPlanner
 
         public void addDriverFactory(boolean inputDriver, boolean outputDriver, PhysicalOperation physicalOperation, OptionalInt driverInstances)
         {
-            addDriverFactory(
-                    inputDriver,
-                    outputDriver,
-                    physicalOperation.getOperatorFactories(),
-                    driverInstances,
-                    physicalOperation.getPipelineExecutionStrategy());
+            List<OperatorFactoryWithTypes> operatorFactoryWithTypes = physicalOperation.getOperatorFactoriesWithTypes();
+            validateFirstOperatorFactory(inputDriver, operatorFactoryWithTypes.get(0).getOperatorFactory(), physicalOperation.getPipelineExecutionStrategy());
+            List<OperatorFactory> operatorFactories = handleLateMaterialization(operatorFactoryWithTypes);
+            driverFactories.add(new DriverFactory(getNextPipelineId(), inputDriver, outputDriver, operatorFactories, driverInstances, physicalOperation.getPipelineExecutionStrategy()));
+        }
+
+        private List<OperatorFactory> handleLateMaterialization(List<OperatorFactoryWithTypes> operatorFactoryWithTypes)
+        {
+            if (isLateMaterializationEnabled(taskContext.getSession())) {
+                return WorkProcessorPipelineSourceOperator.convertOperators(getNextOperatorId(), operatorFactoryWithTypes);
+            }
+            else {
+                return operatorFactoryWithTypes.stream()
+                        .map(OperatorFactoryWithTypes::getOperatorFactory)
+                        .collect(toImmutableList());
+            }
         }
 
         public void addDriverFactory(boolean inputDriver, boolean outputDriver, List<OperatorFactory> operatorFactories, OptionalInt driverInstances, PipelineExecutionStrategy pipelineExecutionStrategy)
         {
+            validateFirstOperatorFactory(inputDriver, operatorFactories.get(0), pipelineExecutionStrategy);
+            driverFactories.add(new DriverFactory(getNextPipelineId(), inputDriver, outputDriver, operatorFactories, driverInstances, pipelineExecutionStrategy));
+        }
+
+        private void validateFirstOperatorFactory(boolean inputDriver, OperatorFactory firstOperatorFactory, PipelineExecutionStrategy pipelineExecutionStrategy)
+        {
             if (pipelineExecutionStrategy == GROUPED_EXECUTION) {
-                OperatorFactory firstOperatorFactory = operatorFactories.get(0);
                 if (inputDriver) {
                     checkArgument(firstOperatorFactory instanceof ScanFilterAndProjectOperatorFactory || firstOperatorFactory instanceof TableScanOperatorFactory);
                 }
@@ -611,12 +626,6 @@ public class LocalExecutionPlanner
                     checkArgument(firstOperatorFactory instanceof LocalExchangeSourceOperatorFactory || firstOperatorFactory instanceof LookupOuterOperatorFactory);
                 }
             }
-
-            if (isLateMaterializationEnabled(taskContext.getSession())) {
-                operatorFactories = WorkProcessorPipelineSourceOperator.convertOperators(getNextOperatorId(), operatorFactories);
-            }
-
-            driverFactories.add(new DriverFactory(getNextPipelineId(), inputDriver, outputDriver, operatorFactories, driverInstances, pipelineExecutionStrategy));
         }
 
         private List<DriverFactory> getDriverFactories()
@@ -746,6 +755,28 @@ public class LocalExecutionPlanner
         public StageExecutionDescriptor getStageExecutionDescriptor()
         {
             return stageExecutionDescriptor;
+        }
+    }
+
+    public static class OperatorFactoryWithTypes
+    {
+        private final OperatorFactory operatorFactory;
+        private final List<Type> types;
+
+        public OperatorFactoryWithTypes(OperatorFactory operatorFactory, List<Type> types)
+        {
+            this.operatorFactory = requireNonNull(operatorFactory, "operatorFactory is null");
+            this.types = requireNonNull(types, "types is null");
+        }
+
+        public OperatorFactory getOperatorFactory()
+        {
+            return operatorFactory;
+        }
+
+        public List<Type> getTypes()
+        {
+            return types;
         }
     }
 
@@ -3241,7 +3272,7 @@ public class LocalExecutionPlanner
      */
     private static class PhysicalOperation
     {
-        private final List<OperatorFactory> operatorFactories;
+        private final List<OperatorFactoryWithTypes> operatorFactoriesWithTypes;
         private final Map<Symbol, Integer> layout;
         private final List<Type> types;
 
@@ -3275,12 +3306,12 @@ public class LocalExecutionPlanner
             requireNonNull(source, "source is null");
             requireNonNull(pipelineExecutionStrategy, "pipelineExecutionStrategy is null");
 
-            this.operatorFactories = ImmutableList.<OperatorFactory>builder()
-                    .addAll(source.map(PhysicalOperation::getOperatorFactories).orElse(ImmutableList.of()))
-                    .add(operatorFactory)
+            this.types = toTypes(layout, typeProvider);
+            this.operatorFactoriesWithTypes = ImmutableList.<OperatorFactoryWithTypes>builder()
+                    .addAll(source.map(PhysicalOperation::getOperatorFactoriesWithTypes).orElse(ImmutableList.of()))
+                    .add(new OperatorFactoryWithTypes(operatorFactory, types))
                     .build();
             this.layout = ImmutableMap.copyOf(layout);
-            this.types = toTypes(layout, typeProvider);
             this.pipelineExecutionStrategy = pipelineExecutionStrategy;
         }
 
@@ -3317,7 +3348,14 @@ public class LocalExecutionPlanner
 
         private List<OperatorFactory> getOperatorFactories()
         {
-            return operatorFactories;
+            return operatorFactoriesWithTypes.stream()
+                    .map(OperatorFactoryWithTypes::getOperatorFactory)
+                    .collect(toImmutableList());
+        }
+
+        private List<OperatorFactoryWithTypes> getOperatorFactoriesWithTypes()
+        {
+            return operatorFactoriesWithTypes;
         }
 
         public PipelineExecutionStrategy getPipelineExecutionStrategy()

core/trino-main/src/test/java/io/trino/operator/TestWorkProcessorPipelineSourceOperator.java
@@ -26,6 +26,7 @@ import io.trino.operator.WorkProcessor.TransformationState;
 import io.trino.operator.WorkProcessorAssertion.Transform;
 import io.trino.spi.Page;
 import io.trino.spi.connector.UpdatablePageSource;
+import io.trino.sql.planner.LocalExecutionPlanner.OperatorFactoryWithTypes;
 import io.trino.sql.planner.plan.PlanNodeId;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -105,7 +106,10 @@ public class TestWorkProcessorPipelineSourceOperator
 
         SourceOperatorFactory pipelineOperatorFactory = (SourceOperatorFactory) getOnlyElement(WorkProcessorPipelineSourceOperator.convertOperators(
                 99,
-                ImmutableList.of(sourceOperatorFactory, firstOperatorFactory, secondOperatorFactory)));
+                ImmutableList.of(
+                        new OperatorFactoryWithTypes(sourceOperatorFactory, ImmutableList.of(BIGINT)),
+                        new OperatorFactoryWithTypes(firstOperatorFactory, ImmutableList.of(BIGINT)),
+                        new OperatorFactoryWithTypes(secondOperatorFactory, ImmutableList.of(BIGINT)))));
 
         DriverContext driverContext = TestingOperatorContext.create(scheduledExecutor).getDriverContext();
         SourceOperator pipelineOperator = pipelineOperatorFactory.createOperator(driverContext);