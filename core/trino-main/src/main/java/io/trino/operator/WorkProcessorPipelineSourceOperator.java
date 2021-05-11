9c23a01490cd803d7df63274702846f142af4aac
Karol Sobczak <napewnotrafi@gmail.com>
2021-05-12 00:27:35 +0200

    Merge output pages after every work processor operator

--- core/trino-main/src/main/java/io/trino/operator/WorkProcessorPipelineSourceOperator.java
+++ core/trino-main/src/main/java/io/trino/operator/WorkProcessorPipelineSourceOperator.java
@@ -86,17 +86,19 @@ public class WorkProcessorPipelineSourceOperator
         }
 
         WorkProcessorSourceOperatorFactory sourceOperatorFactory = (WorkProcessorSourceOperatorFactory) operatorFactoriesWithTypes.get(0).getOperatorFactory();
-        ImmutableList.Builder<WorkProcessorOperatorFactory> workProcessorOperatorFactoriesBuilder = ImmutableList.builder();
+        ImmutableList.Builder<WorkProcessorOperatorFactory> operatorFactoriesBuilder = ImmutableList.builder();
+        ImmutableList.Builder<List<Type>> outputTypesBuilder = ImmutableList.builder();
         int operatorIndex = 1;
         for (; operatorIndex < operatorFactoriesWithTypes.size(); ++operatorIndex) {
             OperatorFactory operatorFactory = operatorFactoriesWithTypes.get(operatorIndex).getOperatorFactory();
             if (!(operatorFactory instanceof WorkProcessorOperatorFactory)) {
                 break;
             }
-            workProcessorOperatorFactoriesBuilder.add((WorkProcessorOperatorFactory) operatorFactory);
+            operatorFactoriesBuilder.add((WorkProcessorOperatorFactory) operatorFactory);
+            outputTypesBuilder.add(operatorFactoriesWithTypes.get(operatorIndex).getTypes());
         }
 
-        List<WorkProcessorOperatorFactory> workProcessorOperatorFactories = workProcessorOperatorFactoriesBuilder.build();
+        List<WorkProcessorOperatorFactory> workProcessorOperatorFactories = operatorFactoriesBuilder.build();
         if (workProcessorOperatorFactories.isEmpty()) {
             return toOperatorFactories(operatorFactoriesWithTypes);
         }
@@ -105,7 +107,7 @@ public class WorkProcessorPipelineSourceOperator
                 .add(new WorkProcessorPipelineSourceOperatorFactory(
                         sourceOperatorFactory,
                         workProcessorOperatorFactories,
-                        operatorFactoriesWithTypes.get(operatorIndex - 1).getTypes(),
+                        outputTypesBuilder.build(),
                         minOutputPageSize,
                         minOutputPageRowCount,
                         maxSmallPagesRowRatio))
@@ -124,7 +126,7 @@ public class WorkProcessorPipelineSourceOperator
             DriverContext driverContext,
             WorkProcessorSourceOperatorFactory sourceOperatorFactory,
             List<WorkProcessorOperatorFactory> operatorFactories,
-            List<Type> outputTypes,
+            List<List<Type>> outputTypes,
             DataSize minOutputPageSize,
             int minOutputPageRowCount,
             double maxSmallPagesRowRatio)
@@ -180,14 +182,15 @@ public class WorkProcessorPipelineSourceOperator
             if (i == operatorFactories.size() - 1) {
                 // materialize output pages as there are no semantics guarantees for non WorkProcessor operators
                 pages = pages.map(Page::getLoadedPage);
-                pages = pages.transformProcessor(processor -> mergePages(
-                        outputTypes,
-                        minOutputPageSize.toBytes(),
-                        minOutputPageRowCount,
-                        maxSmallPagesRowRatio,
-                        processor,
-                        operatorContext.aggregateUserMemoryContext()));
             }
+            List<Type> operatorOutputTypes = outputTypes.get(i);
+            pages = pages.transformProcessor(processor -> mergePages(
+                    operatorOutputTypes,
+                    minOutputPageSize.toBytes(),
+                    minOutputPageRowCount,
+                    maxSmallPagesRowRatio,
+                    processor,
+                    operatorContext.aggregateUserMemoryContext()));
             pages = pages
                     .yielding(() -> operatorContext.getDriverContext().getYieldSignal().isSet())
                     .withProcessEntryMonitor(() -> workProcessorOperatorEntryMonitor(operatorIndex))
@@ -722,7 +725,7 @@ public class WorkProcessorPipelineSourceOperator
     {
         private final WorkProcessorSourceOperatorFactory sourceOperatorFactory;
         private final List<WorkProcessorOperatorFactory> operatorFactories;
-        private final List<Type> outputTypes;
+        private final List<List<Type>> outputTypes;
         private final DataSize minOutputPageSize;
         private final int minOutputPageRowCount;
         private final double maxSmallPagesRowRatio;
@@ -731,7 +734,7 @@ public class WorkProcessorPipelineSourceOperator
         private WorkProcessorPipelineSourceOperatorFactory(
                 WorkProcessorSourceOperatorFactory sourceOperatorFactory,
                 List<WorkProcessorOperatorFactory> operatorFactories,
-                List<Type> outputTypes,
+                List<List<Type>> outputTypes,
                 DataSize minOutputPageSize,
                 int minOutputPageRowCount,
                 double maxSmallPagesRowRatio)
